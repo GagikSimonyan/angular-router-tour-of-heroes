fcf2a9162790b3471f6959c38d000bb4952badf7
Gagik Simonyan <gagik.simonyan92@gmail.com>
2023-09-24 09:03:03 +0400

    Milestone 4 is finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
 import {FormsModule} from "@angular/forms";
 import {HeroesModule} from "./heroes/heroes.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CrisisCenterModule} from "./crisis-center/crisis-center.module";
+import { ComposeMessageComponent } from './compose-message/compose-message.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CrisisListComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ComposeMessageComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     HeroesModule,
+    CrisisCenterModule,
     AppRoutingModule,
   ],
   providers: [],
---
+++ src/app/crisis-center/crisis-detail/crisis-detail.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {Crisis} from "../crisis";
+
+@Component({
+  selector: 'app-crisis-detail',
+  templateUrl: './crisis-detail.component.html',
+  styleUrls: ['./crisis-detail.component.css']
+})
+export class CrisisDetailComponent implements OnInit {
+  crisis!: Crisis;
+  editName = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    // public dialogService: DialogService
+  ) {}
+
+  ngOnInit() {
+    this.route.data
+      .subscribe(data => {
+        const crisis: Crisis = data['crisis'];
+        this.editName = crisis.name;
+        this.crisis = crisis;
+      });
+  }
+
+  cancel() {
+    this.gotoCrises();
+  }
+
+  save() {
+    this.crisis.name = this.editName;
+    this.gotoCrises();
+  }
+
+  gotoCrises() {
+    const crisisId = this.crisis ? this.crisis.id : null;
+    // Relative navigation back to the crises
+    this.router.navigate(['../', { id: crisisId, foo: 'foo' }], { relativeTo: this.route });
+  }
+}
