561af1e6b7d53dc7512e55072c5f0337c01b6bd4
Gagik Simonyan <gagik.simonyan92@gmail.com>
2023-10-25 21:57:28 +0400

    Milestone 5 canActivate guard is finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import {HeroesModule} from "./heroes/heroes.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {CrisisCenterModule} from "./crisis-center/crisis-center.module";
 import { ComposeMessageComponent } from './compose-message/compose-message.component';
+import {AdminModule} from "./admin/admin.module";
+import {AuthModule} from "./auth/auth.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ComposeMessageComponent,
     PageNotFoundComponent,
-    ComposeMessageComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +25,8 @@ import { ComposeMessageComponent } from './compose-message/compose-message.compo
     FormsModule,
     HeroesModule,
     CrisisCenterModule,
+    AdminModule,
+    AuthModule,
     AppRoutingModule,
   ],
   providers: [],
